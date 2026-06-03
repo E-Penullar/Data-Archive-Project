78864d887c3466db32fa709a2b94f02883c02ef4
Erika <erikapenullar@gmail.com>
2026-06-02 15:10:46 -1000

    uploading project1

---
+++ database.h
@@ -0,0 +1,12 @@
+#include "record.h"
+
+int addRecord (struct record **, int, char [ ],char [ ]);
+void printAllRecords(struct record *);
+int findRecord (struct record *, int);
+int deleteRecord(struct record **, int);
+
+int writefile(struct record *, char [ ]);
+int readfile(struct record **, char [ ]);
+void cleanup(struct record **);
+
+
